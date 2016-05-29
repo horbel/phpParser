bdc61ad39d50fa48ec8899dadf0d384f5e31b6b6
horbel <gorbel.alex@ya.ru>
2016-05-30 00:00:16 +0300

    mistake has been corrected

--- parser.php
+++ parser.php
@@ -4,41 +4,45 @@ include_once 'Z:\home\localhost\www\simplehtmldom_1_5\simple_html_dom.php';
 
 $html = file_get_html('http://zestrest.livejournal.com');
 
+
+
+
 class  Source{
     public $notes; //array of objects
     function __construct()
     {
         $this->notes = array();
-
     }
     public function TryParse($html){
         $div_titles = $html->find('div[class=subject] a');
         $div_tour_info = $html->find('div[class=entry_text]');
-        for($i=0; $i<count($div_titles); $i++){
+        for($i=0; $i<3/*count($div_titles)*/; $i++){
             $tempName = $div_titles[$i]->innertext;
             $posBreak = strpos($tempName, ' от ');
             $newName = substr($tempName, 0, $posBreak);
             $this->notes[$i]->name = $newName;
         }
-        for($i=0; i<count($div_tour_info); $i++){
+        for($i=0; $i<3/*count($div_tour_info)*/; $i++){
             $text =  $div_tour_info[$i]->plaintext;
             //$separatedText = preg_split("/\$/", $text, -1, PREG_SPLIT_NO_EMPTY|PREG_SPLIT_DELIM_CAPTURE);
             $separatedText = explode(',', $text);
-
+            //$this->notes[$i]->hotelNames = array();
             $this->notes[$i]->hotelNames[] =   $separatedText[0];
+            //echo $separatedText[0]."<br>";
             for ($j = 0; $j<count($separatedText); $j++){
 
-                $arr = str_split($separatedText[$j]);
+                //$arr = str_split($separatedText[$j]);
                 //$temp = preg_grep("/*$/", $arr);
-
+                $newStr = preg
 
                 //GET ALLMOST EVERY FIELDs HERE
 
 
+                echo $arr;
                 //echo $separatedText[$j];
-                //echo '<br>';
+                echo '<br>';
             }
-           // echo '<hr>';
+            echo '<hr>';
 
 
         }
@@ -77,7 +81,7 @@ class Note{
 }
 $s = new Source();
 $s->TryParse($html);
-$s->showInfo();
+//$s->showInfo();
 
 
 /*foreach ($div_titles as $el){
